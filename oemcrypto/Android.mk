01c0502b09d54d65e34c80c30de553feae40cc66
Mingliang Yang <mingliang.yang@amlogic.com>
2013-05-27 10:46:56 +0800

    PD#70980 support widevine L1 +secureos default L3

--- oemcrypto/Android.mk
+++ oemcrypto/Android.mk
@@ -3,10 +3,18 @@ LOCAL_PATH:= $(call my-dir)
 ifeq ($(TARGET_ARCH),arm)
 
 #####################################################################
-# libdrmwvmplugin.so
+# libdrmwvmplugin?.so
 include $(CLEAR_VARS)
+include $(TOP)/vendor/widevine/oemcryptolevel.mk
 
+ifeq ($(LOCAL_OEMCRYPTO_LEVEL),1)
+LOCAL_MODULE := libdrmwvmplugin_L$(LOCAL_OEMCRYPTO_LEVEL)
+endif
+
+ifeq ($(LOCAL_OEMCRYPTO_LEVEL),3)
 LOCAL_MODULE := libdrmwvmplugin
+endif
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/drm
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -17,10 +25,18 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_PREBUILT)
 
 #####################################################################
-# libwvm.so
+# libwvm?.so
 include $(CLEAR_VARS)
+include $(TOP)/vendor/widevine/oemcryptolevel.mk
 
+ifeq ($(LOCAL_OEMCRYPTO_LEVEL),1)
+LOCAL_MODULE := libwvm_L$(LOCAL_OEMCRYPTO_LEVEL)
+endif
+
+ifeq ($(LOCAL_OEMCRYPTO_LEVEL),3)
 LOCAL_MODULE := libwvm
+endif
+
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_SRC_FILES := $(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
---
+++ secureos/lib/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+include $(TOP)/vendor/widevine/oemcryptolevel.mk
+ifeq ($(LOCAL_OEMCRYPTO_LEVEL),1)
+ifeq ($(TARGET_ARCH),arm)
+
+LOCAL_MODULE := libwvsecureos_api
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES := $(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_STRIP_MODULE := true
+
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
+
+endif
+endif
