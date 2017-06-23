a8752dacb5cac37cecd713ea318b74562f1f3a1a
Rongrong Zhou <rongrong.zhou@amlogic.com>
2017-06-23 10:08:53 +0800

    PD#145255 : WIDEVINE generate amlogic files to vendor/
    Change-Id: I29c710e724f4441f69cc5e60b8dbb946cec964dc

--- libwvdrmengine/Android.mk
+++ libwvdrmengine/Android.mk
@@ -6,7 +6,12 @@ ifneq ($(filter arm arm64, $(TARGET_ARCH)),)
 # libdrmdecrypt.so
 include $(CLEAR_VARS)
 LOCAL_MODULE := libwvdrmengine
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/mediadrm
+else
 LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/lib/mediadrm
+endif
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_SRC_FILES := $(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
