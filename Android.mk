91d589f22a2b2944819981c4510ff6c6a63df283
Jian Wang <jian.wang@amlogic.com>
2017-08-04 15:11:26 +0800

    drm: modify secure dst buf to native handle for o
    PD# NONE
    
    Change-Id: I3ae96eaac44f694a8aa3662e434c2e2383f895dc

--- Android.mk
+++ Android.mk
@@ -1,7 +1,9 @@
 # widevine prebuilts only available for ARM
 ifneq ($(filter arm arm64,$(TARGET_ARCH)),)
 
+ifeq (,$(wildcard vendor/amlogic/widevine))
 LOCAL_32_BIT_ONLY := true
 include $(call all-subdir-makefiles)
+endif
 
 endif # TARGET_ARCH == arm
