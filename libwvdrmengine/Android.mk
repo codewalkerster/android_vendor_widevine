47bd30cec93fc7ea871ebe04db2a3f9615c3e6e1
rongrong zhou <rongrong.zhou@amlogic.com>
2018-03-05 16:41:32 +0800

    Widevine: move AML WV/SECMEM library to libmediadrm [1/3]
    PD# 160940
    
    google cdm library -> vendor/widevine.git
    wv ca/ta, secmem ca/ta -> libmediadrm.git
    
    Change-Id: Iab2d73b2e043afdffa5743738df37587549b3a2d

--- libwvdrmengine/Android.mk
+++ libwvdrmengine/Android.mk
@@ -29,44 +29,5 @@ LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-ifeq ($(LOCAL_OEMCRYPTO_LEVEL),1)
-LOCAL_MODULE := liboemcrypto
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_SRC_FILES := $(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_STRIP_MODULE := false
-LOCAL_32_BIT_ONLY := true
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_PREBUILT)
-endif
-
-include $(CLEAR_VARS)
-ifeq ($(LOCAL_OEMCRYPTO_LEVEL),1)
-LOCAL_MODULE := e043cde0-61d0-11e5-9c260002a5d5c51b
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/teetz
-LOCAL_MODULE_SUFFIX := .ta
-ifeq ($(TARGET_ENABLE_TA_SIGN), true)
-$(info $(shell mkdir -p $(shell pwd)/$(PRODUCT_OUT)/signed/libwvdrmengine))
-$(info $(shell $(shell pwd)/$(BOARD_AML_VENDOR_PATH)/tdk/ta_export/scripts/sign_ta_auto.py \
-		--in=$(shell pwd)/$(LOCAL_PATH)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX) \
-		--out=$(shell pwd)/$(PRODUCT_OUT)/signed/libwvdrmengine/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)))
-ifeq (,$(wildcard vendor/amlogic/ampere))
-LOCAL_SRC_FILES := ../../../$(PRODUCT_OUT)/signed/libwvdrmengine/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
-else
-LOCAL_SRC_FILES := ../../../../../$(PRODUCT_OUT)/signed/libwvdrmengine/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
-endif
-else
-LOCAL_SRC_FILES := $(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
-endif
-LOCAL_STRIP_MODULE := false
-LOCAL_MODULE_TAGS := optional
-LOCAL_32_BIT_ONLY := true
-include $(BUILD_PREBUILT)
-endif
-
 endif # TARGET_ARCH == arm
 
